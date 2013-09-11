9b914e4fa4ec1ff767708b17931ea3d2547f44bd
Riagms <riyariya44@gmail.com>
2013-09-11 15:24:45 +0530

    cmt

--- EmployeeHrPortal/SecondRaceViewController.h
+++ EmployeeHrPortal/SecondRaceViewController.h
@@ -7,7 +7,39 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "racemdl.h"
+@interface SecondRaceViewController : UIViewController{
+    BOOL recordResults;
+}
 
-@interface SecondRaceViewController : UIViewController
+@property(strong,nonatomic)racemdl*racemdl1;
+/*xmlparser*/
+
+@property(strong,nonatomic)NSXMLParser *xmlParser;
+@property(strong,nonatomic)NSMutableString *soapResults;
+@property(strong,nonatomic)NSMutableData *webData;
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollview;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *agencysgmnt;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *campaignsgmt;
+
+@property (strong, nonatomic) IBOutlet UISegmentedControl *Affirmatvesgmt;
+
+@property (strong, nonatomic) IBOutlet UISegmentedControl *activedutysgmnt;
+
+@property (strong, nonatomic) IBOutlet UISegmentedControl *reservistsgmnt;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *departmentsgmnt;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *seperatedsegmnt;
+
+
+@property(readwrite)NSInteger IsProtectedVeteranValue;
+@property(readwrite)NSInteger IsDisablevalue;
+@property(readwrite)NSInteger IsVietnamEravalue;
+@property(readwrite)NSInteger IsActiveReservistvalue;
+@property(readwrite)NSInteger IsDisabledVeteranvalue;
+@property(readwrite)NSInteger IsSeperatedVeteranvalue;
+
+
+- (IBAction)submitbtn:(id)sender;
 
 @end
--- EmployeeHrPortal/SecondRaceViewController.m
+++ EmployeeHrPortal/SecondRaceViewController.m
@@ -27,12 +27,178 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    _scrollview.frame=CGRectMake(0, 0, 320, 548);
+     [ _scrollview setContentSize:CGSizeMake(320,850)];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+}
+#pragma webservice
+-(void)UpdateApplicantInformations
+{
+    recordResults = FALSE;
+    NSString *soapMessage;
+    
+    soapMessage = [NSString stringWithFormat:
+                   
+                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                   
+                   
+                   "<soap:Body>\n"
+                   
+                   "<UpdateApplicantInformations xmlns=\"http://webserv.kontract360.com/\">\n"
+                   "<ApplicantId>%d</ApplicantId>\n"
+                   "<IsConvict>%d</IsConvict>\n"
+                   "<ConvictExplanation>%@</ConvictExplanation>\n"
+                   "<TWICNumber>%@</TWICNumber>\n"
+                   "<AgeLimit>%d</AgeLimit>\n"
+                   "<LegelRights>%d</LegelRights>\n"
+                   "<WorkedOverTime>%d</WorkedOverTime>\n"
+                   "<WorkedEarlier>%d</WorkedEarlier>\n"
+                   "<WorkedPeriod>%@</WorkedPeriod>\n"
+                   "<WorkOutofTown>%d</WorkOutofTown>\n"
+                   "<Referred>%@</Referred>\n"
+                   "<ReferredAgency>%@</ReferredAgency>\n"
+                   "<IsProtectedVeteran>%d</IsProtectedVeteran>\n"
+                   "<IsDisable>%d</IsDisable>\n"
+                   "<IsVietnamEra>%d</IsVietnamEra>\n"
+                   "<IsActiveReservist>%d</IsActiveReservist>\n"
+                   "<IsDisabledVeteran>%d</IsDisabledVeteran>\n"
+                   "<IsSeperatedVeteran>%d</IsSeperatedVeteran>\n"
+                   "</UpdateApplicantInformations>\n"
+                   "</soap:Body>\n"
+                   "</soap:Envelope>\n",_applicantId,_isconvictvalue,_convictExplanationText.text,_twicnumberText.text,_agelimitvalue,_legalrightsvalue,_workedovertimevalue,_workedearliervalue,_workedperiodText.text,_workoutoftownvalue,_refferbtn.titleLabel.text,_refferedagencyText.text,_IsProtectedVeteranValue,_IsDisablevalue,_IsVietnamEravalue,_IsActiveReservistvalue,_IsDisabledVeteranvalue,_IsSeperatedVeteranvalue];
+    NSLog(@"soapmsg%@",soapMessage);
+    
+    
+    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
+    NSURL *url = [NSURL URLWithString:@"http://webserv.kontract360.com/service.asmx"];
+    
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+    
+    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    [theRequest addValue: @"http://webserv.kontract360.com/UpdateApplicantInformations" forHTTPHeaderField:@"Soapaction"];
+    
+    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    
+    if( theConnection )
+    {
+        _webData = [NSMutableData data];
+    }
+    else
+    {
+        ////NSLog(@"theConnection is NULL");
+    }
+    
 }
 
+
+#pragma mark - Connection
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+	[_webData setLength: 0];
+}
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+   	[_webData appendData:data];
+}
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    UIAlertView *  Alert=[[UIAlertView alloc]initWithTitle:@"Alert" message:@"ERROR with theConenction" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+    
+    [Alert show];
+}
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"DONE. Received Bytes: %d", [_webData length]);
+	NSString *theXML = [[NSString alloc] initWithBytes: [_webData mutableBytes] length:[_webData length] encoding:NSUTF8StringEncoding];
+	NSLog(@"xml===== %@",theXML);
+	
+	
+	if( _xmlParser )
+	{
+		
+	}
+	
+	_xmlParser = [[NSXMLParser alloc] initWithData: _webData];
+	[_xmlParser setDelegate:(id)self];
+	[_xmlParser setShouldResolveExternalEntities: YES];
+	[_xmlParser parse];
+    
+}
+
+
+
+- (IBAction)submitbtn:(id)sender {
+    
+      if(_ agencysgmnt.selectedSegmentIndex==0)
+    {
+        _IsProtectedVeteranValue=1;
+    }
+    else if(_agencysgmnt.selectedSegmentIndex==1)
+    {
+        _IsProtectedVeteranValue=0;
+    }
+    if(_ campaignsgmt.selectedSegmentIndex==0)
+    {
+        _IsDisablevalue=1;
+    }
+    else if(_campaignsgmt.selectedSegmentIndex==1)
+    {
+        _IsDisablevalue=0;
+    }
+    if(_activedutysgmnt.selectedSegmentIndex==0)
+    {
+        _IsVietnamEravalue=1;
+    }
+    else if(_activedutysgmnt.selectedSegmentIndex==1)
+    {
+        _IsVietnamEravalue=0;
+    }
+    if(_reservistsgmnt.selectedSegmentIndex==0)
+    {
+        _IsActiveReservistvalue=1;
+    }
+    else if(_reservistsgmnt.selectedSegmentIndex==1)
+    {
+        _IsActiveReservistvalue=0;
+    }
+    if(_departmentsgmnt.selectedSegmentIndex==0)
+    {
+        _IsDisabledVeteranvalue=1;
+    }
+    else if(_departmentsgmnt.selectedSegmentIndex==1)
+    {
+        _IsDisabledVeteranvalue=0;
+    }
+    
+    if(_seperatedsegmnt.selectedSegmentIndex==0)
+    {
+        _IsSeperatedVeteranvalue=1;
+    }
+    else if(_seperatedsegmnt.selectedSegmentIndex==1)
+    {
+        _IsSeperatedVeteranvalue=0;
+    }
+    
+
+    
+    
+    
+}
 @end
