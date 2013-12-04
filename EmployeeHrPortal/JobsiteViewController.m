f18692463c8cebc8572f0bcb3be140a03d592254
Riagms <riyariya44@gmail.com>
2013-12-04 14:27:47 +0530

    cmttd

--- EmployeeHrPortal/JobsiteViewController.m
+++ EmployeeHrPortal/JobsiteViewController.m
@@ -63,6 +63,8 @@
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     buttonclicked=0;
+    //[self.navigationController.navigationBar setHidden:YES];
+
     self.navigationController.navigationBar.tintColor=[[UIColor alloc]initWithRed:16/255.0f green:78/255.0f blue:139/255.0f alpha:1];
     UIBarButtonItem *logoutbutton=[[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"logout1"] style:UIBarButtonItemStylePlain target:self action:@selector(logoutAction)];
     
